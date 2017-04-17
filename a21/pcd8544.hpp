ebf4d9530ec43b8e72cdc2eb0344a998165cc62a
Aleh Dzenisiuk <aleh@mediamonks.com>
2017-04-17 02:24:18 +0200

    Add simple PCD8544 console

--- a21/pcd8544.hpp
+++ a21/pcd8544.hpp
@@ -199,14 +199,14 @@ public:
   
   /** 
    * Transports a bunch of bytes for the given row. Each byte is responsible for a 8 pixel column within the row 
-   * (LSB is in the bottom of the row, MSB is in the top).
+   * (MSB is in the bottom of the row, LSB is in the top).
    *
    * The row is filled from left to right (i.e. column address is automaticallt incremented).
    *                    col      col + 1
-   *  line row * 8:     # bit 7  # bit 7  ...
-   *  line row * 8 + 1: # bit 6  # bit 7  ...
+   *  line row * 8:     # bit 0  # bit 0  ...
+   *  line row * 8 + 1: # bit 1  # bit 1  ...
    *     ...
-   *  line row * 8 + 7: # bit 0  # bit 0  ...
+   *  line row * 8 + 7: # bit 7  # bit 7  ...
    *                    ^        ^
    *                    byte 0   byte 1
    *
@@ -223,81 +223,224 @@ public:
     endWriting();
   }
   
-  static void writeText(const uint8_t *font, uint8_t col, uint8_t row, uint8_t max_width, const char *text) {
+  static void fillRow(uint8_t col, uint8_t row, uint8_t length, uint8_t filler) {
+    beginWriting();
+    setAddressInternal(col, row);
+    for (uint8_t c = length; c > 0; c--) {
+      write(Data, filler);
+    }
+    endWriting();
+  }
+  
+  //
+  // Fonts
+  //
+  /** Returns the width of the glyph corresponding to a character in the given font and, if a buffer is provided, 
+  * copies glyph's bitmap into it. */
+  static uint8_t dataForCharacter(PCD8544Font font, char ch, uint8_t *buffer) {
+ 
+    const uint8_t *p = font;
+
+    uint8_t options = pgm_read_byte(p++);
+    if ((options & 1) && 'a' <= ch && ch <= 'z') {
+      ch = ch - 'a' + 'A';
+    }
+
+    while (true) {
+
+      // First character in the range (0 would mean no more ranges are defined).
+      uint8_t first = pgm_read_byte(p++);
+      if (first == 0) 
+        break;
+
+      // Last character in the range.
+      uint8_t last = pgm_read_byte(p++);
+
+      // Number of bytes every character in the range occupies. 
+      uint8_t bytes_per_character = pgm_read_byte(p++);
+
+      if (first <= ch && ch <= last) {
+        
+        // Our character is in the range, let's copy the data for it.
+        
+        p += (ch - first) * bytes_per_character;
+        // The first byte of the glyph data is the actual width of the glyph.
+        uint8_t width = pgm_read_byte(p++);
+        // Copy the bitmap if the caller expects it.
+        if (buffer) {
+          memcpy_PF(buffer, (uint_farptr_t)p, width);
+        }
+
+        return width;
+      }
+
+      // Well, let's skip to the next range of characters.
+      p += (last + 1 - first) * bytes_per_character;      
+    }
+
+    // Let's return data for sort of a default character.
+    return dataForCharacter(font, '?', buffer);
+  }  
+
+  static uint8_t textWidth(PCD8544Font font, const char *text) {
+    
+    char ch;
+    const char *src = text;
+    uint8_t result = 0;
+    while ((ch = *src++)) {      
+        uint8_t width = dataForCharacter(font, ch, NULL);
+        result += width + 1;
+    }
+    
+    return result;
+  }
+  
+  /** Returns how many characters will fit max_width without being truncacted. */
+  static uint8_t numberOfCharsFittingWidth(PCD8544Font font, const char *text, uint8_t max_width) {
+    
+    uint8_t result = 0;
+    
+    char ch;
+    const char *src = text;
+    uint8_t total_width = 0;
+    while ((ch = *src++)) {
+      uint8_t new_total_width = total_width + dataForCharacter(font, ch, NULL) + 1;
+      if (new_total_width > max_width)
+        break;
+      total_width = new_total_width;
+      result++;
+    }
+    
+    return result;
+  }
+  
+  static uint8_t drawText(
+    PCD8544Font font, 
+    const uint8_t col, 
+    const uint8_t row, 
+    const uint8_t max_width, 
+    const char *text, 
+    const uint8_t xor_mask = 0
+  ) {
     
     beginWriting();
     
     setAddressInternal(col, row);
     
-    uint8_t bitmap[8];
     char ch;
     const char *src = text;
     uint8_t width_left = max_width;
     while ((ch = *src++)) {
       
-        uint8_t width = PCD8544Font::dataForCharacter(font, ch, bitmap);
+        uint8_t bitmap[8];
+        uint8_t width = dataForCharacter(font, ch, bitmap);
         
         for (uint8_t i = 0; i < width; i++) {
-          
-          write(Data, 0xFE & ~bitmap[i]);
-          
+          write(Data, bitmap[i] ^ xor_mask);
           if (--width_left == 0) {
             endWriting();
             return;
           }
         }
         
-        write(Data, 0xFE);
+        write(Data, xor_mask);
         if (--width_left == 0)
           break;
     }
     
     endWriting();
+    
+    return max_width - width_left;
   }
 };
 
-template<typename lcd, typename font = PCD8544FontPixelstadTweaked>
+template<typename lcd, typename font>
 class PCD8544Console {
   
 private:
+  
+  static const uint8_t MaxCols = lcd::Cols / 4;
+  
+  char _buffer[lcd::Rows][MaxCols + 1];
   uint8_t _row;
   uint8_t _col;
+  uint8_t _rowWidth;
+  uint8_t _filledRows;
+  
+  void newline() {
     
+    _col = 0;
+    _rowWidth = 0;
+    
+    _row++;
+    if (_row >= lcd::Rows) {
+      _row = 0;
+    }
+    _filledRows++;
+    if (_filledRows >= lcd::Rows) {
+      _filledRows--;
+    }
+    _buffer[_row][_col] = 0;
+  }
+  
+  void carriage_return() {
+    _col = 0;
+    _rowWidth = 0;
+  }
+
 public:
   
-  PCD8544Console() : _row(0), _col(0) {}
-    
+  PCD8544Console() : _row(0), _col(0), _rowWidth(0), _filledRows(0) {}
+  
   void clear() {
-    _row = _col = 0;
+    _row = _filledRows = 0;
+    _col = 0;
+    _rowWidth = 0;
+    for (uint8_t row = 0; row < lcd::Rows; row++) {
+      _buffer[row][0] = 0;
+    }
     lcd::clear();
   }
   
-  void print(const char *str) {
+  void draw() {
     
-    char ch;
-    uint8_t bitmap[8];
-    const char *src = str;
-    while ((ch = *src++)) {
+    for (uint8_t i = 0; i <= _filledRows; i++) {
       
-      if (ch < 32)
-        continue;
-
-      uint8_t width = PCD8544Font::dataForCharacter(font::data(), ch, bitmap);
+      int8_t row_index = _row - _filledRows + i;
+      if (row_index < 0)
+        row_index += lcd::Rows;
       
-      if (_col + width < lcd::Cols) {
-        lcd::writeRow(_col, _row, bitmap, width);
-        _col += width + 1;
-      } else {
-        _col = 0;
-        _row++;
-        if (_row >= lcd::Rows) {
-          _row = 0;
-        }
-        lcd::writeRow(_col, _row, bitmap, width);
-        _col += width + 1;
-      }
+      // Print the row and erase the rest.
+      uint8_t width = lcd::drawText(font::font(), 0, i, lcd::Cols, _buffer[row_index]);
+      lcd::fillRow(width, i, lcd::Cols - width, 0);
     }
   }
+
+  void print(const char *text) {
+    
+    char ch;
+    const char *src = text;
+    while ((ch = *src++)) {
+      
+      if (ch >= ' ') {
+        
+        uint8_t width = lcd::dataForCharacter(font::font(), ch, NULL);
+        if (_col >= MaxCols || _rowWidth + width >= lcd::Cols) {
+          newline();        
+        }
+      
+        _buffer[_row][_col] = ch;
+        _col++;
+        _buffer[_row][_col] = 0;
+        _rowWidth += width + 1;
+        
+      } else if (ch == '\n') {
+        newline();
+      } else if (ch == '\r') {
+        carriage_return();
+      }
+    }
+  }    
 };
 
 } // namespace
